01cd22c9ffdb79b022927e1782b6d58685bddf6e
jovanbankovic <jovan.bankovic@elfak.rs>
2021-12-24 19:06:03 +0100

    /

--- Wall.py
+++ Wall.py
@@ -1,4 +1,8 @@
 from Colors import Colors
+from copy import copy, deepcopy
+
+from Player import Player
+from TableFields import TableFields
 
 
 class Wall(object):
@@ -20,6 +24,64 @@ class Wall(object):
         self.wall_type = wall_type
         self.position = position
         return self
+
+    def deep_copy_table(self, table, x, y):
+        w, h = y, x
+        matrix = [[0 for x in range(w)] for y in range(h)]
+        for i in range(x):
+            for j in range(y):
+                matrix[i][j] = deepcopy(table[i][j])
+        return matrix
+
+    def playMove(self, obj, pos, player, figure):
+        new_table_fields = TableFields()
+        field_copy = self.deep_copy_table(obj.table_fields, obj.x, obj.y)
+        new_player1 = Player(obj.player1.first_figure_pos_x,
+                             obj.player1.first_figure_pos_y,
+                             obj.player1.second_figure_pos_x,
+                             obj.player1.second_figure_pos_y,
+                             obj.player1.remaining_wals)
+        new_player2 = Player(obj.player2.first_figure_pos_x,
+                             obj.player2.first_figure_pos_y,
+                             obj.player2.second_figure_pos_x,
+                             obj.player2.second_figure_pos_y,
+                             obj.player2.remaining_wals)
+        new_table_fields.create_game_table(field_copy, obj.x, obj.y, obj.k, new_player1, new_player2)
+
+        if player == 'player1':
+            if figure == 'figure1':
+                new_table_fields.player1.figure1.positionX = pos[0]
+                new_table_fields.player1.figure1.positionY = pos[1]
+            elif figure == 'figure2':
+                new_table_fields.player1.figure2.positionX = pos[0]
+                new_table_fields.player1.figure2.positionY = pos[1]
+        elif player == 'player2':
+            if figure == 'figure1':
+                new_table_fields.player2.figure1.positionX = pos[0]
+                new_table_fields.player2.figure1.positionY = pos[1]
+            elif figure == 'figure2':
+                new_table_fields.player2.figure2.positionX = pos[0]
+                new_table_fields.player2.figure2.positionY = pos[1]
+
+        return new_table_fields
+
+    def findPossibleMoves(self, obj, current_position, previous_position):
+        #nalazi sve moguce poteze ki se ne vracaju u prev. pos
+        #vraca listu sledecih mogucih position-a
+        return
+
+    def calculate_closed_path_to_starting_positions(self, starting_position_x, starting_position_y, matrix, currentPos, previousPos):
+        """
+        if startpos
+            return true
+        posMs = findPossMovies(matrix, currentPos, previousPos)
+        if !posMs or posMs.length == 0
+            return false
+        bool fined = false
+        foreach move in posMs
+            finded = finded or calculate_closed_path_to_start_position(starting_position_x, starting_position_y, playMove(matrix, move), move, currentPosition)
+        return finded
+        """
 
     def init_wall(self, obj, player):
         """
@@ -57,6 +119,7 @@ class Wall(object):
                         return -1
                     else:
                         player.remainingBlueWalls = player.remainingBlueWalls - 1
+                        self.calculate_closed_path_to_starting_positions(obj, pos)
                         obj.update_field_for_blue(x, y, y+1, wall)
                 else:
                     print(Colors.WARNING + "You don't have any more blue walls." + Colors.ENDC)
@@ -73,6 +136,7 @@ class Wall(object):
                         print(Colors.WARNING + "There is a blue wall between." + Colors.ENDC)
                         return -1
                     else:
+                        self.calculate_closed_path_to_starting_positions(obj, pos)
                         obj.update_field_for_green(x, y, x+1, wall)
                 else:
                     print(Colors.WARNING + "You don't have any more green walls." + Colors.ENDC)
